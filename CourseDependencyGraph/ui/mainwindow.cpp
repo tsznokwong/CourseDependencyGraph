d7becfca1a5feefe6b10f09f4c643f99abfdf6f4
frankwangyufei <frankwangyufei559@gmail.com>
2018-11-26 20:32:45 +0800

    adds treeview to mainwindow

--- CourseDependencyGraph/ui/mainwindow.cpp
+++ CourseDependencyGraph/ui/mainwindow.cpp
@@ -5,12 +5,36 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
     dependencyManager(new DependencyManager) {
-    ui->setupUi(this);
+	ui->setupUi(this);
 
     this->dependencyManager->loadCSV();
+
+	setupTreeView();
 }
+
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setupTreeView(){
+	ui->treeWidget->setColumnCount(1);
+}
+
+void MainWindow::treeViewAddRoot(QString string){
+	QTreeWidgetItem *item = new QTreeWidgetItem(ui->treeWidget);
+	item->setText(0, string);
+	treeViewAddChild(item, "asdfasdfa");
+	treeViewAddChild(item, "asdfasdfa");
+	treeViewAddChild(item, "asdfasdfa");
+	treeViewAddChild(item, "asdfasdfa");
+	ui->treeWidget->addTopLevelItem(item);
+}
+
+void MainWindow::treeViewAddChild(QTreeWidgetItem *parent, QString string){
+	QTreeWidgetItem *item = new QTreeWidgetItem();
+	item->setText(0, string);
+	parent->addChild(item);
 }
